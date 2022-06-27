45a5713916b4dbd38e5542f5f06014d9841508c9
Robert Daley <robertdaley@gmail.com>
2022-06-27 11:01:36 +0100

    Lives

--- cargame.py
+++ cargame.py
@@ -11,6 +11,7 @@ right_lane = width/2 + road_w/4
 left_lane = width/2 - road_w/4
 # animation parameters
 speed = 1
+lives = 3
 
 # initiallize the app
 pygame.init()
@@ -26,49 +27,52 @@ screen.fill((60, 220, 0))
 pygame.display.update()
 
 # load player vehicle
-car = pygame.image.load("unicorn.png")
+unicorn = pygame.image.load("unicorn.png")
 #resize image
-car = pygame.transform.scale(car, (250, 250))
-car_loc = car.get_rect()
-car_loc.center = right_lane, height*0.8
+unicorn = pygame.transform.scale(unicorn, (250, 250))
+unicorn_loc = unicorn.get_rect()
+unicorn_loc.center = right_lane, height*0.8
 
 # load enemy vehicle
-car2 = pygame.image.load("shark.png")
-car2_loc = car2.get_rect()
-car2_loc.center = left_lane, height*0.2
+shark = pygame.image.load("shark.png")
+shark_loc = shark.get_rect()
+shark_loc.center = left_lane, height*0.2
 
 # Load life symbol
 heart = pygame.image.load("life_symbol.png")
+heart = pygame.transform.scale(heart, (30, 30))
 heart_loc1 = heart.get_rect()
 heart_loc2 = heart.get_rect()
 heart_loc3 = heart.get_rect()
-heart_loc1.center = (645, 25)
-heart_loc2.center = (705, 25)
-heart_loc3.center = (765, 25)
+heart_loc1.center = (730, 25)
+heart_loc2.center = (760, 25)
+heart_loc3.center = (790, 25)
 
 counter = 0
 # game loop
 while running:
     counter += 1
-
     # increase game difficulty overtime
-    if counter == 5000:
-        speed += 0.15
-        counter = 0
-        print("level up", speed)
+    # if counter == 5000:
+    #     speed += 0.15
+    #     counter = 0
+    #     print("level up", speed)
 
     # animate enemy vehicle
-    car2_loc[1] += speed
-    if car2_loc[1] > height:
+    shark_loc[1] += speed
+    if shark_loc[1] > height:
         # randomly select lane
         if random.randint(0,1) == 0:
-            car2_loc.center = right_lane, -200
+            shark_loc.center = right_lane, -200
         else:
-            car2_loc.center = left_lane, -200
+            shark_loc.center = left_lane, -200
 
     # end game logic
     # TODO remove this logic and use lives to drive endgame
-    if car_loc[0] == car2_loc[0] and car2_loc[1] > car_loc[1] - 250:
+    print(unicorn_loc[0], unicorn_loc[1]) 
+    print(shark_loc[0], shark_loc[1])
+
+    if unicorn_loc[0] == shark_loc[0] and shark_loc[1] > unicorn_loc[1] - 250:
         print("GAME OVER! YOU LOST!")
         break
 
@@ -80,11 +84,17 @@ while running:
         if event.type == KEYDOWN:
             # move user car to the left
             if event.key in [K_a, K_LEFT]:
-                car_loc = car_loc.move([-int(road_w/2), 0])
+                unicorn_loc = unicorn_loc.move([-int(road_w/2), 0])
             # move user car to the right
             if event.key in [K_d, K_RIGHT]:
-                car_loc = car_loc.move([int(road_w/2), 0])
-    
+                unicorn_loc = unicorn_loc.move([int(road_w/2), 0])
+
+
+
+    screen.blit(heart, heart_loc1)
+    screen.blit(heart, heart_loc2)
+    screen.blit(heart, heart_loc3)
+
     # draw road
     pygame.draw.rect(
         screen,
@@ -106,14 +116,14 @@ while running:
         (255, 255, 255),
         (width/2 + road_w/2 - roadmark_w*3, 0, roadmark_w, height))
 
+
     # place car images on the screen
-    screen.blit(car, car_loc)
-    screen.blit(car2, car2_loc)
-    screen.blit(heart, heart_loc1)
-    screen.blit(heart, heart_loc2)
-    screen.blit(heart, heart_loc3)
+    screen.blit(unicorn, unicorn_loc)
+    screen.blit(shark, shark_loc)
+
     # apply changes
     pygame.display.update()
 
 # collapse application window
+print('Did you have fun?')
 pygame.quit()
